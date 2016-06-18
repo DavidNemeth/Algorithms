53a97c5a6c842eb899c329ebe2b8281a318b00f0
DavidNemeth <david.nemeth90@gmail.com>
2016-06-18 21:22:39 +0200

    QuickFind-Tree

---
+++ DynamicConnectivity/QuickFind/QuickFind.cs
@@ -0,0 +1,37 @@
+﻿namespace QuickFind
+{
+    public class QuickFind
+    {
+        private int[] id;
+
+        public QuickFind(int n)
+        {
+            id = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return id[p] == id[q];
+        }        
+
+        public void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            id[i] = j;
+        }
+
+        private int Root(int i)
+        {
+            while (i != id[i])
+            {
+                i = id[i];
+            }
+            return i;
+        }
+    }
+}
