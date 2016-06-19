2847fe166d15e6f070cb1a8e8e6334b5de5242bb
DavidNemeth <david.nemeth90@gmail.com>
2016-06-19 20:57:06 +0200

    Path Compression

--- DynamicConnectivity/QuickFind/QuickFind.cs
+++ DynamicConnectivity/QuickFind/QuickFind.cs
@@ -3,6 +3,7 @@
     public class QuickFind
     {
         private int[] id;
+        private int[] numberOfItem;
 
         public QuickFind(int n)
         {
@@ -13,22 +14,36 @@
             }
         }
 
-        public bool Connected(int p, int q)
+        public bool Find(int p, int q)
         {
-            return id[p] == id[q];
-        }        
+            return Root(p) == Root(q);
+        }
 
         public void Union(int p, int q)
         {
             int i = Root(p);
             int j = Root(q);
-            id[i] = j;
+            if (i == j)
+            {
+                return;
+            }
+            if (numberOfItem[i] < numberOfItem[j])
+            {
+                id[i] = j;
+                numberOfItem[j] += numberOfItem[i];
+            }
+            else
+            {
+                id[j] = i;
+                numberOfItem[i] += numberOfItem[j];
+            }
         }
 
         private int Root(int i)
         {
             while (i != id[i])
             {
+                id[i] = id[id[i]];
                 i = id[i];
             }
             return i;
