702ff6206d2248d243d3f681cece7c3cc70f4722
DavidNemeth <david.nemeth90@gmail.com>
2016-06-28 05:32:42 +0200

    stack

---
+++ BasicAlgorithms/StackAPI/FixedCapacityStackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace StackAPI
+{
+    public class FixedCapacityStackOfStrings
+    {
+        private string[] s;
+        private int N = 0;
+
+        public FixedCapacityStackOfStrings(int capacity)
+        {
+            s = new string[capacity];
+        }
+
+        public bool isEmpty()
+        {
+            return N == 0;
+        }
+
+        public void Push(string item)
+        {
+            s[N++] = item;
+        }
+
+        public string pop()
+        {
+            return s[--N];
+        }
+    }
+}
---
+++ BasicAlgorithms/StackAPI/StackOfStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StackAPI
+{
+    public class StackOfStrings
+    {       
+        private Node first = null;
+
+        private class Node
+        {
+            internal string item;
+            internal Node next;
+        }
+
+        public bool isEmpty()
+        {
+            return first == null;
+        }
+
+        public void push(string item)
+        {
+            Node oldfirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = oldfirst;
+        }
+
+        public string pop()
+        {
+            string item = first.item;
+            first = first.next;
+            return item;
+        }
+    }
+}
