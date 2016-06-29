8d0c80927daf9bbe11b784d975452146570d16dc
DavidNemeth <david.nemeth90@gmail.com>
2016-06-29 08:25:12 +0200

    ArraysAndStrings

---
+++ BasicAlgorithms/ArraysAndStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Solutions.IsUnique("asdfghjklqwertzuiopőú"));
+            Console.WriteLine(Solutions.PermutationCount("123456789100asdfghhjxyacyqqwe"));
+        }
+    }
+}
---
+++ BasicAlgorithms/ArraysAndStrings/Solutions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStrings
+{
+    public static class Solutions
+    {
+        public static bool IsUnique(string input)
+        {
+            bool[] array = new bool[512];
+            foreach (var item in input)
+            {
+                if (array[(int)item])
+                {
+                    return false;
+                }
+                else
+                {
+                    array[(int)item] = true;
+                }
+            }
+            return true;
+        }
+
+        public static bool Permutation(string string1, string string2)
+        {
+            if (string1.Length != string2.Length)
+            {
+                return false;
+            }
+            char[] first = string1.ToArray();
+            char[] second = string2.ToArray();
+            Array.Sort(first);
+            Array.Sort(second);
+            if (first.ToString() != second.ToString())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static long PermutationCount(string input)
+        {
+            int count = 0;
+            bool[] barray = new bool[512];
+            foreach (var item in input)
+            {
+                barray[item] = true;
+            }
+            foreach (var item in barray)
+            {
+                if (item)
+                {
+                    count++;
+                }
+            }
+            long result = count;
+            for (int i = 1; i < count; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
