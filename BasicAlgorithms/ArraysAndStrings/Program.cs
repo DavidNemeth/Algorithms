6ceb269a18ddbf1ac8dc7d977abb1829d6c79084
DavidNemeth <david.nemeth90@gmail.com>
2016-06-30 06:44:47 +0200

    Perm

--- BasicAlgorithms/ArraysAndStrings/Program.cs
+++ BasicAlgorithms/ArraysAndStrings/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArraysAndStrings
 {
@@ -11,10 +7,10 @@ namespace ArraysAndStrings
         static void Main(string[] args)
         {
             Solutions sol = new Solutions();
-
-            Console.WriteLine(sol.IsUnique("asdfghjklqwertzuiopőú"));
-            Console.WriteLine(sol.PermutationCount("123456789100asdfghhjxyacyqqwe"));
-            Console.WriteLine("Mr John Smith     ".GenerateSlug());
+            //Console.WriteLine(sol.IsUnique("asdfghjklqwertzuiopőú"));
+            //Console.WriteLine(sol.PermutationCount("123456789100asdfghhjxyacyqqwe"));
+            //Console.WriteLine("Mr John Smith     ".GenerateSlug());
+            Console.WriteLine(sol.IsPalindromePermutation("Tact Coa "));
         }
     }
 }
--- BasicAlgorithms/ArraysAndStrings/Solutions.cs
+++ BasicAlgorithms/ArraysAndStrings/Solutions.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArraysAndStrings
 {
     public class Solutions
     {
+        //O(n)
         public bool IsUnique(string input)
         {
             bool[] array = new bool[512];
@@ -22,6 +21,55 @@ namespace ArraysAndStrings
                     array[(int)item] = true;
                 }
             }
+            return true;
+        }
+
+        //O(n^2)
+        public bool IsPalindromePermutation(string input)
+        {
+            bool unevenFound = false;
+            char uneven = ' ';
+
+            int count = 0;
+
+            //Remove whitespace from the string.
+            string ret = "";
+            foreach (var item in input)
+            {
+                if (char.IsLetterOrDigit(item))
+                {
+                    ret += item;
+                }
+            }
+            ret = ret.ToLower();
+            // Check each letter to see if there's an even number of it.
+            for (int i = 0; i < ret.Length; i++)
+            {
+                count = 0;
+                for (int j = 0; j < ret.Length; j++)
+                {
+                    if (ret[j] == ret[i])
+                    {
+                        count++;
+                    }
+                }
+                // If there was an odd number of those entries
+                // and an uneven is already found, then a palindrome
+                // is impossible, so return false.
+                if (count % 2 == 1)
+                {
+                    if (unevenFound == true && uneven != ret[i])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        unevenFound = true;
+                        uneven = ret[i];   
+                    }
+                }
+            }
+            // If we made it all the way through that loop without returning false, then
             return true;
         }
 
@@ -42,6 +90,7 @@ namespace ArraysAndStrings
             return true;
         }
 
+        //O(n)
         public long PermutationCount(string input)
         {
             int count = 0;
@@ -63,6 +112,49 @@ namespace ArraysAndStrings
                 result *= i;
             }
             return result;
-        }        
+        }
+
+        //O(n)
+        private bool IsPalindrome(string input)
+        {
+            string result = "";
+            foreach (var item in input)
+            {
+                if (char.IsLetterOrDigit(item))
+                {
+                    result += item;
+                }
+            }
+            result = result.ToLower();
+            string reverse = new string(result.ToCharArray().Reverse().ToArray());
+
+            if (result == reverse)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //O(n^n)?
+        private List<string> Permutations(string input)
+        {
+            var ret = new List<string>();
+            if (input.Length == 1)
+            {
+                ret.Add(input);
+            }
+            else
+            {
+                foreach (var item in input)
+                {
+                    var perms = input.Remove(input.IndexOf(item), 1);
+                    foreach (var perm in Permutations(perms))
+                    {
+                        ret.Add(item + perm);
+                    }
+                }
+            }
+            return ret;
+        }
     }
 }
