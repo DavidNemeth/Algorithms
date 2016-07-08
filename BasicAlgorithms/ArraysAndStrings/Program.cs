1ce206e18f8d9a71acddc208dc13a219cf80a54c
DavidNemeth <david.nemeth90@gmail.com>
2016-07-08 03:31:51 +0200

    BubbleSort

--- BasicAlgorithms/ArraysAndStrings/Program.cs
+++ BasicAlgorithms/ArraysAndStrings/Program.cs
@@ -8,19 +8,18 @@ namespace ArraysAndStrings
     {
         static void Main(string[] args)
         {
-            int[] iArr = new int[10] { 1, 3, 5, 2, 7, 6, 24, 12, 61, 0 };
+            int[] iArr = new int[10] { 5, 2, 3, 6, 1, 3, 87, 1, 6, 0 };
             IComparable[] mSort = new IComparable[10] { 5, 2, 7, 3, 21, 6, 52, 62, 2, 6 };
             Solutions sol = new Solutions();
             SortAlgs sort = new SortAlgs();
             SearchAlgs search = new SearchAlgs();
-            //Console.WriteLine(sol.IsUnique("asdfghjklqwertzuiopőú"));
-            //Console.WriteLine(sol.PermutationCount("123456789100asdfghhjxyacyqqwe"));
-            //Console.WriteLine("Mr John Smith     ".GenerateSlug());
-            //Console.WriteLine(sol.IsPalindromePermutation("Tact Coa "));
-            //Console.WriteLine(sol.Distance("pale", "ple"));
-            //Console.WriteLine(sol.StringCompression("aabcccccaaa"));
-            //Console.WriteLine(sol.Inversion(iArr));
-            search.LinearSearch(iArr, 7);
+
+            sort.BubbleSort(iArr);
+            foreach (var item in iArr)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
--- BasicAlgorithms/ArraysAndStrings/SortAlgs.cs
+++ BasicAlgorithms/ArraysAndStrings/SortAlgs.cs
@@ -8,5 +8,25 @@ namespace ArraysAndStrings
 {
     public class SortAlgs
     {
+        public void BubbleSort(int[] input)
+        {
+            for (int i = input.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        swapValues(ref input[j],ref input[j + 1]);
+                    }
+                }
+            }
+        }
+
+        private void swapValues(ref int indexOne, ref int indexTwo)
+        {
+            int temp = indexOne;
+            indexOne = indexTwo;
+            indexTwo = temp;
+        }
     }
 }
