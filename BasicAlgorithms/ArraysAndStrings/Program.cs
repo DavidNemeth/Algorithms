be5de9c28de020518026e57824d8a5593da3541e
DavidNemeth <david.nemeth90@gmail.com>
2016-07-08 03:09:40 +0200

    LinearSearch

--- BasicAlgorithms/ArraysAndStrings/Program.cs
+++ BasicAlgorithms/ArraysAndStrings/Program.cs
@@ -8,9 +8,11 @@ namespace ArraysAndStrings
     {
         static void Main(string[] args)
         {
-            int[] iArr = new int[6] { 1, 3, 5, 2, 7, 6 };
+            int[] iArr = new int[10] { 1, 3, 5, 2, 7, 6, 24, 12, 61, 0 };
             IComparable[] mSort = new IComparable[10] { 5, 2, 7, 3, 21, 6, 52, 62, 2, 6 };
             Solutions sol = new Solutions();
+            SortAlgs sort = new SortAlgs();
+            SearchAlgs search = new SearchAlgs();
             //Console.WriteLine(sol.IsUnique("asdfghjklqwertzuiopőú"));
             //Console.WriteLine(sol.PermutationCount("123456789100asdfghhjxyacyqqwe"));
             //Console.WriteLine("Mr John Smith     ".GenerateSlug());
@@ -18,7 +20,7 @@ namespace ArraysAndStrings
             //Console.WriteLine(sol.Distance("pale", "ple"));
             //Console.WriteLine(sol.StringCompression("aabcccccaaa"));
             //Console.WriteLine(sol.Inversion(iArr));
-            Console.WriteLine(sol.Inversion(iArr));
+            search.LinearSearch(iArr, 7);
         }
     }
 }
---
+++ BasicAlgorithms/ArraysAndStrings/SearchAlgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArraysAndStrings
+{
+    public class SearchAlgs
+    {
+        public void LinearSearch(int[] input, int value)
+        {
+            int index = 0;
+            bool found = false;
+
+            foreach (var item in input)
+            {
+                if (item == value)
+                {
+                    index = Array.IndexOf(input, item);
+                    found = true;
+                }
+            }
+
+            if (found)
+            {
+                Console.WriteLine("found value of {0}, at the index: {1}", value, index);
+            }
+            else
+            {
+                Console.WriteLine("value {0} not found in the input array", value);
+            }
+        }
+    }
+}
