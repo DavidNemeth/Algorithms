e154542dee46c7878c1e8448bbb5a260211e39aa
DavidNemeth <david.nemeth90@gmail.com>
2016-07-09 10:58:02 +0200

    ArrayExtensions

--- BasicAlgorithms/ArraysAndStrings/Program.cs
+++ BasicAlgorithms/ArraysAndStrings/Program.cs
@@ -19,7 +19,6 @@ namespace ArraysAndStrings
                 Console.WriteLine(item);
             }
             search.BinarySearch(iArr, 6);
-
         }
     }
 }
---
+++ BasicAlgorithms/Extensions/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static void fill<T>(this T[] originalArray, T with)
+        {
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                originalArray[i] = with;
+            }
+        }
+    }
+}
