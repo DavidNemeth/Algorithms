336183c929343753206efc7a0e4e4585e7b64967
DavidNemeth <david.nemeth90@gmail.com>
2016-07-25 21:09:35 +0200

    Exercises

---
+++ BasicAlgorithms/Exercises/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reinforcement re = new Reinforcement();
+            Console.WriteLine(re.Punctuation("Let’s try, Mike!"));
+        }
+    }
+}
+
---
+++ BasicAlgorithms/Exercises/Reinforcement.cs
@@ -0,0 +1,65 @@
+﻿using Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class Reinforcement
+    {
+        public void Entries()
+        {
+
+            int[] gameEntriesA = new int[5] { 1, 2, 3, 4, 5 };
+            int[] gameEntriesB = gameEntriesA;
+            gameEntriesB[4] = 550;
+            Console.WriteLine(gameEntriesA[4]);
+        }
+
+        public bool IsMultiple(long n, long m)
+        {
+            return n % m == 0;
+        }
+        public bool IsEven(int i)
+        {
+            return (i & 1) == 0;
+        }
+        public int SumAll(int n)
+        {
+            int x = 0;
+            int total = 0;
+            while (x <= n)
+            {
+                total += x;
+                x++;
+            }
+            return total;
+        }
+        public int Vowels(string input)
+        {
+            int ret = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+                {
+                    ret++;
+                }
+            }
+            return ret;
+        }
+        public string Punctuation(string input)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (var item in input)
+            {
+                if (char.IsLetterOrDigit(item) || char.IsWhiteSpace(item))
+                {
+                    ret.Append(item);
+                }
+            }
+            return ret.ToString();
+        }
+    }
+}
