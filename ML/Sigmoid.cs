a75ebcd48e4ac19e24eab33706d5ae3def6bfa50
Dávid Németh <david.nemeth90@gmail.com>
2016-10-24 04:41:52 +0200

    Update Sigmoid.cs

--- ML/Sigmoid.cs
+++ ML/Sigmoid.cs
@@ -2,3 +2,9 @@ public double Sigmoid(double x)
 {
     return 2 / (1 + Math.Exp(-2 * x)) - 1;
 }
+
+public double Derivative(double x)
+{
+    double s = Sigmoid(x);
+    return 1 - (Math.Pow(s, 2));
+}
