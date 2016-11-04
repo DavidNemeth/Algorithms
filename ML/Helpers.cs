3248808cd06f0577375f3e7d97047556137be08d
Dávid Németh <david.nemeth90@gmail.com>
2016-11-04 01:55:10 +0100

    Update Helpers.cs

--- ML/Helpers.cs
+++ ML/Helpers.cs
@@ -1,26 +1,12 @@
-public double Sigmoid(double x)
+public static class Sigmoid
 {
-    return 1 / (1 + Math.Exp(-x));
-}
+    public static double Output(double x)
+    {
+        return x < -45.0 ? 0.0 : x > 45.0 ? 1.0 : 1.0 / (1.0 + Math.Exp(-x));
+    }
 
-public double Derivative(double x)
-{
-    double s = Sigmoid(x);
-    return s * (1 - s);
+    public static double Derivative(double x)
+    {
+        return x * (1 - x);
+    }
 }
-
- private static int MaxIndex(double[] vector) // helper for Accuracy()
- {
-     // index of largest value
-     int bigIndex = 0;
-     double biggestVal = vector[0];
-     for (int i = 0; i < vector.Length; ++i)
-     {
-         if (vector[i] > biggestVal)
-         {
-             biggestVal = vector[i];
-             bigIndex = i;
-         }
-     }
-     return bigIndex;
- }
